7d6384a7fbc096a26907aeefce4911e022a340ed
creallfluharty <creallfluharty@gmail.com>
2025-04-30 22:37:02 -0500

    initial commit: maven repo with gson

---
+++ src/main/java/edu/missouri/groupn/AdoptMe/App.java
@@ -0,0 +1,9 @@
+package edu.missouri.groupn.AdoptMe;
+
+import com.google.gson.Gson;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
